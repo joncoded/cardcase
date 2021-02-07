27f035fd33d674ba40d77900a56940fb66bc85a0
joncoded <joncoded@gmail.com>
2021-02-07 00:10:55 -0500

    updating sample styling

--- src/components/SearchPanel/CardCase/Card.js
+++ src/components/SearchPanel/CardCase/Card.js
@@ -6,12 +6,21 @@ const Card = (props) => {
   if (data) {
     return (
 
-      /* make card template here */
+      /* make card template based on your JSON-based data here */
             
       <div className="card">
-        <h2>{data.name}</h2>
-      </div>
+        <h2>
+          [{data.topLevelDomain[0]}] &nbsp; 
+          <img src={data.flag} alt="" style={{maxHeight: "20px", maxWidth: "40px"}} /> &nbsp; 
+          <a href={ "https://en.wikipedia.org/wiki/" + data.name.replace(/ /g, '_')} target="_blank">
+            {data.name}
+          </a>
+        </h2>
 
+        { data.capital ? <p><strong>Capital</strong>: {data.capital} </p> : ""}
+        <p><strong>Population</strong>: {data.population.toLocaleString()} </p>
+      </div>
+    
     )
   }
 
